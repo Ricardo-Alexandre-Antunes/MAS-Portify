e02d6a5b894cf51fcda86dfcac08e245c629ec3e
Ricardo-Alexandre-Antunes <ricardo.alexandre.antunes@ua.pt>
2023-01-04 11:38:14 +0000

    json

--- scripts/home.js
+++ scripts/home.js
@@ -6,3 +6,16 @@ new Swiper('.swiper-container', {
     initialSlide: 3,
     slideToClickedSlide: true,
 });
+
+$(document).ready(function () {
+    alert("AAAAAAAAAAAAAAaa");
+    var url = "organizations.json";
+    $.getJSON(url, function (data) {
+    console.log(data)
+    alert(data[0].Id);
+    alert(data[0].Events);
+    alert(data[1].District);
+    alert(data[1].Pricholder);
+    });
+        
+});
