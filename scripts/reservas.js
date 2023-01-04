55da7203a30ce8029b1f88256b508778f8ef1db0
Ricardo-Alexandre-Antunes <ricardo.alexandre.antunes@ua.pt>
2023-01-04 18:12:04 +0000

    json, reservedetails, ko bindings

--- scripts/reservas.js
+++ scripts/reservas.js
@@ -27,7 +27,7 @@ var dict = {
                     "Preço": 2
                 }
             ],
-            "Image": "./Imagens/Reservas/bowling.jog"
+            "Image": "./Imagens/Reservas/bowling.jpg"
         },
         {
             "Id": 2,
@@ -158,7 +158,7 @@ var dict = {
                     "Preço": 25
                 }
             ],
-            "Image": "./Imagens/Reservas/fearzone.jpg"
+            "Image": "./Imagens/Reservas/fearzone.png"
         },
         {
             "Id": 6,
@@ -319,25 +319,254 @@ var dict = {
                     "Preço": 280
                 }
             ],
-            "Image": "./Imagens/Reservas/fintas.jpg"
+            "Image": "./Imagens/Reservas/marina.jpg"
         },
         {
-            "Id": 10
+            "Id": 10,
+            "Name": "Partyspot",
+            "District": "Lisboa",
+            "Priceholder": 215,
+            "Events": [
+                {
+                    "Id": 1,
+                    "Name": "Vamos Dançar!",
+                    "Preço": 215
+                },
+                {
+                    "Id": 2,
+                    "Name": "Vamos à Discoteca!",
+                    "Preço": 250
+                },
+                {
+                    "Id": 3,
+                    "Name": "Party Glow!",
+                    "Preço": 280
+                },
+                {
+                    "Id": 4,
+                    "Name": "Dança Com Tatuagem Glitter!",
+                    "Preço": 305
+                },
+                {
+                    "Id": 5,
+                    "Name": "Disco Kids Silver!",
+                    "Preço": 250
+                },
+                {
+                    "Id": 6,
+                    "Name": "Disco Kids Gold!",
+                    "Preço": 285
+                },
+                {
+                    "Id": 7,
+                    "Name": "Live Concert!",
+                    "Preço": 395
+                },
+                {
+                    "Id": 8,
+                    "Name": "Vamos Dançar e Fazer Uma Pintura Facial!",
+                    "Preço": 305
+                },
+                {
+                    "Id": 9,
+                    "Name": "Let's be a POPSTAR",
+                    "Preço": 305
+                },
+                {
+                    "Id": 10,
+                    "Name": "Festa da Jasmin",
+                    "Preço": 305
+                },
+                {
+                    "Id": 11,
+                    "Name": "A Dança do Panda!",
+                    "Preço": 305
+                },
+                {
+                    "Id": 12,
+                    "Name": "Festa da Minnie!",
+                    "Preço": 305
+                },
+                {
+                    "Id": 13,
+                    "Name": "Segurança é com o Marshall! (Patrulha Pata)",
+                    "Preço": 305
+                },
+                {
+                    "Id": 14,
+                    "Name": "Os Descendentes",
+                    "Preço": 305
+                },
+                {
+                    "Id": 15,
+                    "Name": "A Festa do Frozen!",
+                    "Preço": 305
+                },
+                {
+                    "Id": 16,
+                    "Name": "A Festa da Bela!",
+                    "Preço": 305
+                },
+                {
+                    "Id": 17,
+                    "Name": "O Baile da Cinderela!",
+                    "Preço": 305
+                },
+                {
+                    "Id": 18,
+                    "Name": "Vamos ajudar a Rapunzel?",
+                    "Preço": 305
+                },
+                {
+                    "Id": 19,
+                    "Name": "Espetáculo de magia!",
+                    "Preço": 390
+                },
+                {
+                    "Id": 20,
+                    "Name": "A Dança da Fantasia",
+                    "Preço": 250
+                },
+                {
+                    "Id": 21,
+                    "Name": "Disco Party",
+                    "Preço": 250
+                },
+                {
+                    "Id": 22,
+                    "Name": "Disco Glow Party",
+                    "Preço": 285
+                },
+                {
+                    "Id": 23,
+                    "Name": "DJ Spot",
+                    "Preço": 340
+                },
+                {
+                    "Id": 24,
+                    "Name": "Glow Party + DJ Spot",
+                    "Preço": 340
+                },
+                {
+                    "Id": 24,
+                    "Name": "Live Concert!",
+                    "Preço": 340
+                },
+            ],
+            "Image": "./Imagens/Reservas/Party-Spot.webp"
         },
         {
-            "Id": 11
+            "Id": 11,
+            "Name": "To be kid",
+            "District": "Lisboa",
+            "Priceholder": 200,
+            "Events": [
+                {
+                    "Id": 1,
+                    "Name": "KID1",
+                    "Preço": 200
+                },
+                {
+                    "Id": 2,
+                    "Name": "KID2",
+                    "Preço": 225
+                },
+                {
+                    "Id": 3,
+                    "Name": "KID3",
+                    "Preço": 280
+                }
+            ],
+            "Image": "./Imagens/Reservas/tobekid.webp"
         },
         {
-            "Id": 12
-        },
-        {
-            "Id": 13
-        },
-        {
-            "Id": 14
-        },
-        {
-            "Id": 15
+            "Id": 11,
+            "Name": "Casa do Impacto",
+            "District": "Lisboa",
+            "Priceholder": 120,
+            "Events": [
+                {
+                    "Id": 1,
+                    "Name": "Jantares - Sala Infinita",
+                    "Preço": 800
+                },
+                {
+                    "Id": 2,
+                    "Name": "Cocktails - Páteo",
+                    "Preço": 600
+                },
+                {
+                    "Id": 3,
+                    "Name": "Cocktails - Sala da Bica",
+                    "Preço": 500
+                },
+                {
+                    "Id": 4,
+                    "Name": "Cocktails - Terraço (Rooftop)",
+                    "Preço": 400
+                },
+                {
+                    "Id": 5,
+                    "Name": "Cocktails - Sala Infinita",
+                    "Preço": 600
+                },
+                {
+                    "Id": 6,
+                    "Name": "Eventos Culturais - Sala da Bica",
+                    "Preço": 500
+                },
+                {
+                    "Id": 7,
+                    "Name": "Eventos Culturais - Sala Infinita",
+                    "Preço": 600
+                },
+                {
+                    "Id": 8,
+                    "Name": "Eventos Académicos - Sala da Bica",
+                    "Preço": 400
+                },
+                {
+                    "Id": 9,
+                    "Name": "Eventos Académicos - Sala Reuniões",
+                    "Preço": 120
+                },
+                {
+                    "Id": 10,
+                    "Name": "Eventos Académicos - Sala Infinita",
+                    "Preço": 500
+                },
+                {
+                    "Id": 11,
+                    "Name": "Eventos Sociais - Sala da Bica",
+                    "Preço": 500
+                },
+                {
+                    "Id": 12,
+                    "Name": "Eventos Sociais - Terraço (Rooftop)",
+                    "Preço": 400
+                },
+                {
+                    "Id": 13,
+                    "Name": "Eventos Sociais - Sala Infinita",
+                    "Preço": 600
+                },
+                {
+                    "Id": 14,
+                    "Name": "Utilização de espaço para filmagens - Páteo",
+                    "Preço": 800
+                },
+                {
+                    "Id": 15,
+                    "Name": "Utilização de espaço para filmagens - Sala da Bica",
+                    "Preço": 600
+                },
+                {
+                    "Id": 16,
+                    "Name": "Utilização de espaço para filmagens - Sala Infinita",
+                    "Preço": 700
+                },
+            ],
+            "Image": "./Imagens/Reservas/impacto.jpg"
         }
     ]
 }
