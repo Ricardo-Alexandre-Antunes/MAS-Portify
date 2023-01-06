15c57047580f3fc3ae4600876925a62b952fa731
alof04 <hpclafonso@gmail.com>
2023-01-06 01:07:18 +0000

    .

--- scripts/carrinho.js
+++ scripts/carrinho.js
@@ -3,7 +3,8 @@ function adicionar(elmn) {
     var nome = document.getElementById("company").innerHTML;
     var atividade = document.getElementById(ids + "-tab")
     var ppp = document.getElementById("preco_fixo" + ids)
-    var np = document.getElementById("")
-
-
+    var np = document.getElementById('output' + IdS).innerHTML
+    var pt = document.getElementById('price' + ids).innerHTML
+    items += { "nome": nome, "atividade": atividade, "ppp": ppp, "np": np, "pt": pt }
 }
+console.log(items)
