5ea61d696744dd475d2e63b315237480704a1b74
Ricardo-Alexandre-Antunes <ricardo.alexandre.antunes@ua.pt>
2023-01-06 01:32:29 +0000

    ...

--- scripts/reservasdetails.js
+++ scripts/reservasdetails.js
@@ -72,32 +72,6 @@ ko.bindingHandlers.trimedValue = {
     };
 
 $(document).ready(function () {
-<<<<<<< Updated upstream
-=======
-    id = getUrlParameter('id');
-    boingerino = boing[id - 1];
-    info = boingerino.Info;
-    events = info.Events;
-    console.log(events.Preco)
-    console.log(id);
-    console.log(info);
-    console.log(boingerino);
-    console.log(events);
-    self.Events(events);
-    self.Id(events.Id);
-    self.Name(events.Name);
-    self.Price(events.Preco);
-    self.Description(events.Descricao);
-    console.log(events.Descricao);
-    console.log(info.Name);
-    self.OrgName(info.Name);
-    self.Image(info.Image);
-    console.log(info.Image);
-    var map = { "first": "1", "second": "2" };
-    console.log(getKeyByValue(dict, "2"));
-    console.log(dict);
-    console.log(boing[getKeyByValue("Id", 1)]);
->>>>>>> Stashed changes
     console.log("ready!");
     ko.applyBindings();
         
@@ -112,32 +86,13 @@ function sliderino(elem) {
     console.log('preco' + ids)
     console.log(document.getElementById('preco' + ids).innerHTML)
     console.log(document.getElementById('precoadwa'))
-    console.log(document.getElementById('myRange' + ids))
     console.log(document.getElementById('output' + ids))
     document.getElementById('output' + ids).innerHTML = document.getElementById('preco' + ids).value;
 }
 
-<<<<<<< Updated upstream
 function preco_total(elem) {
     var ids = elem.parentNode.parentNode.id
     console.log(document.getElementById('price' + ids).value)
     document.getElementById('price' + ids).value = document.getElementById('preco_fixo' + ids).innerHTML * document.getElementById('preco' + ids).value;
     document.getElementById('price' + ids).innerHTML = document.getElementById('preco_fixo' + ids).innerHTML * document.getElementById('preco' + ids).value + 'â‚¬';
-}
-=======
-var slider = document.getElementById("myRange");
-console.log(slider.value)
-var output = document.getElementById("output");
-console.log(output)
-document.getElementById("output").innerHTML = slider.value; // Display the default slider value
->>>>>>> Stashed changes
-
-// Update the current slider value (each time you drag the slider handle)
-function sliderino () {
-    document.getElementById("output").innerHTML = document.getElementById("myRange").value;
-    console.log(document.getElementById("myRange").value)
-}
-
-function preco_total() {
-    document.getElementById("price").innerHTML = document.getElementById("preco").innerHTML * document.getElementById("myRange").value + '€'
 }
