b74f955aed58525e61340106da68fa6b72f8e49c
alof04 <hpclafonso@gmail.com>
2023-01-06 00:49:28 +0000

    .

---
+++ scripts/carrinho.js
@@ -0,0 +1,9 @@
+function adicionar(elmn) {
+    ids = elem.parentNode.parentNode.id
+    var nome = document.getElementById("company").innerHTML;
+    var atividade = document.getElementById(ids + "-tab")
+    var ppp = document.getElementById("preco_fixo" + ids)
+    var np = document.getElementById("")
+
+
+}
